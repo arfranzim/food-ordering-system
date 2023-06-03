492c1e156a52b7aaa7fa5898a39946bbae7a532e
andre <andrefranzim@gmail.com>
2023-06-03 17:54:51 -0300

    Created doamin core. [Root, AggregateRoot]

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.food.ordering.system.domain.entity;
+
+import java.util.Objects;
+
+public abstract class BaseEntity<ID> {
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity<?> that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
